154f40a322e5f138da83d8b3fc20204c6ea6836a
Oliver Gibbs <02oliverg.64@gmail.com>
2025-02-05 14:15:40 -0500

    Added Weather() - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,4 +1,15 @@
-# Programmer: Oliver Gibbs
-# Branch: Weather
-# Date:
-# Program:
+print("\n***********************************\n")
+
+print("Weather Branch - Developer: Oliver Gibbs\n")
+
+#Import Libraries Here!
+import random
+from time import sleep
+
+# Weather Function to determine the weather
+def weather():
+    weatherForecastList = ["snowing", "blizzard", "icy" "rainy" "windy", "sunny"]
+    weatherCondition = random.choice(weatherForecastList)
+    return weatherCondition
+
+print(weather())
