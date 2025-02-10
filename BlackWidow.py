c52a207bf55b7e868151a8d79ddb85b42692ac07
oliverrg64 <oliverr.g64@gmail.com>
2025-02-10 13:49:31 -0500

    Created VRS( ) - Snowy - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -8,8 +8,14 @@ from time import sleep
 
 # Weather Function to determine the weather
 def weather():
-    weatherForecastList = ["snowing", "blizzard", "icy" "rainy" "windy", "sunny"]
+    weatherForecastList = ["snowy", "blizzard", "icy", "rainy", "windy", "sunny"]
     weatherCondition = random.choice(weatherForecastList)
     return weatherCondition
 
-print(weather())
+weatherAlert = weather()
+
+def vehicleResponseSystem():
+    if weatherAlert == "snowy":
+        print("\nThe National Weather Service has updated your alarm by 30 minutes because of the forecast of", weatherAlert, "weather condition")
+
+vehicleResponseSystem()
