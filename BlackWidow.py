2efc3da06055f3df8e462c5aba864ed99bf3e808
Oliver Gibbs <02oliverg.64@gmail.com>
2025-02-14 14:13:19 -0500

    Added gasStationList and Print Function

--- BlackWidow.py
+++ BlackWidow.py
@@ -5,5 +5,12 @@ import random
 from time import sleep
 
 def gasLevelGauge():
-  gasLevelList = ["Empty", "Low", "Quarter Tank", "Half Tank", "Three-Quarter Tank", "Full Tank"]
-  return random.choice(gasLevelList) 
+    gasLevelList = ["Empty", "Low", "Quarter Tank", "Half Tank", "Three-Quarter Tank", "Full Tank"]
+    return random.choice(gasLevelList)
+
+def gasStations():
+    gasStationsList = ["Shell","Marathon","Speedway","Circle K","Wesco","Meijer","Buc-ees","Sam's Club","Costco"]
+    return random.choice(gasStationsList)
+
+print(gasLevelGauge())
+print(gasStations())
